35cda8160640b17e925515c01e4483b19fab1bdd
Charlotte-R <49107073+Charlotte-R-01134@users.noreply.github.com>
2025-03-21 15:58:47 +0000

    Fixed new pytorch error

--- fastervit/models/registry.py
+++ fastervit/models/registry.py
@@ -160,7 +160,7 @@ def is_model_pretrained(model_name):
 
 def load_state_dict(checkpoint_path, use_ema=False):
     if checkpoint_path and os.path.isfile(checkpoint_path):
-        checkpoint = torch.load(checkpoint_path, map_location='cpu')
+        checkpoint = torch.load(checkpoint_path, map_location='cpu', weights_only=False)
         state_dict_key = 'state_dict'
         if isinstance(checkpoint, dict):
             if use_ema and 'state_dict_ema' in checkpoint:
